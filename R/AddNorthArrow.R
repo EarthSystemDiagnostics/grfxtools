f3b69ec928d2bb97ddd6badc1da2a19a84d1416b
Thomas Münch <tmuench@awi.de>
2023-08-03 14:28:47 +0200

    AddNorthArrow: settable text size and adjustment.

--- R/AddNorthArrow.R
+++ R/AddNorthArrow.R
@@ -1,24 +1,38 @@
-# Code taken from function addnortharrow in package prettymapr v0.2.4:
+# Code adapted from function addnortharrow in package prettymapr v0.2.4:
 # GPL-2
 # Copyright (C) 2022 Dewey Dunnington
 
+# Changes to original code by Thomas Muench on 2023/08/03:
+# - enable user setting of text size for "N"
+# - rename text.col to col.text
+# - enable user label position adjustment relative to north arrow
+# - revised function documentation
+
 #' Plot north arrow
 #'
-#' Plot a north arrow (pointing directly "up") positioned based on current plot
-#' extents.
+#' Plot a north arrow (pointing directly "up" and with label "N") positioned
+#' based on current plot extents.
 #'
 #' @param pos where to align the north arrow. One of "bottomleft",
 #'   "bottomright", "topleft", or "topright".
 #' @param padin a vector of length 2 determining the distance in inches between
 #'   the scalebar and the edge of the plottable area.
-#' @param scale scale the default north arrow to make it bigger or smaller.
+#' @param scale scale the north arrow to make it bigger or smaller.
 #' @param lwd the line width outlining the north arrow.
 #' @param border the line color outlining the north arrow.
 #' @param cols a vector of length 2 determining the two colors to be drawn for
 #'   the north arrow.
-#' @param text.col color of the "N".
-#' @author Dewey Dunnington
-#' @source <https://CRAN.R-project.org/package=prettymapr>
+#' @param col.text color of the text label.
+#' @param cex.text character expansion factor for the text label. This is an
+#'   absolute measure, not scaled by \code{par("cex")} or by setting
+#'   \code{par("mfrow")} or \code{par("mfcol")}.
+#' @param vadj.text value in [0, 1] to specify the vertical adjustment of the
+#'   text label relative to the north arrow, with 0 for top, 1 for bottom, and
+#'   0.5 for centered position. On most devices values outside [0, 1] will also
+#'   work.
+#' @author Dewey Dunnington, Thomas Münch
+#' @source Adapted from
+#'   <https://CRAN.R-project.org/package=prettymapr>
 #' @examples
 #'
 #' plot(1 : 5, 1 : 5, asp = 1)
@@ -27,7 +41,8 @@
 #' @export
 AddNorthArrow <- function (pos = "topright", padin = c(0.15, 0.15), scale = 1,
                            lwd = 1, border = "black",
-                           cols = c("white", "black"), text.col = "black")
+                           cols = c("white", "black"), col.text = "black",
+                           cex.text = 1, vadj.text = 0.5)
 {
   extents <- graphics::par("usr")
   arrow1.x <- c(-0.5, 0, 0)
@@ -36,7 +51,6 @@ AddNorthArrow <- function (pos = "topright", padin = c(0.15, 0.15), scale = 1,
   arrow2.y <- arrow1.y
   htin <- 0.6 * scale
   wdin <- 0.6 * scale
-  text.cex <- 1.25 * scale
   bottomin <- graphics::grconvertY(extents[3], from = "user", 
                                    to = "inches")
   leftin <- graphics::grconvertX(extents[1], from = "user", 
@@ -45,7 +59,7 @@ AddNorthArrow <- function (pos = "topright", padin = c(0.15, 0.15), scale = 1,
                                 to = "inches")
   rightin <- graphics::grconvertX(extents[2], from = "user", 
                                   to = "inches")
-  textheight <- graphics::strheight("N", units = "user", cex = text.cex)
+  textheight <- graphics::strheight("N", units = "user", cex = cex.text) ## <- tmuench, 2023/08/03
   if (pos == "bottomleft") {
     x <- graphics::grconvertX(leftin + padin[1], from = "inches", 
                               to = "user")
@@ -86,6 +100,7 @@ AddNorthArrow <- function (pos = "topright", padin = c(0.15, 0.15), scale = 1,
   graphics::polygon(x - adj[1] * wd + arrow2.x * wd + wd/2, 
                     y - adj[2] * ht + arrow2.y * ht + ht/2, border = border, 
                     lwd = lwd, col = cols[2])
-  graphics::text(x - adj[1] * wd + wd/2, y - adj[2] * ht + 
-                                         ht * 0, "N", cex = text.cex, adj = c(0.5, 0.5), col = text.col)
+  graphics::text(x - adj[1] * wd + wd/2, y - adj[2] * ht + ht * 0, "N",
+                 cex = cex.text, adj = c(0.5, vadj.text), col = col.text)
+  ## <- tmuench, 2023/08/03
 }
